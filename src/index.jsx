35a87c94146f88aa6d3e9a16c8dfdc284a6562de
Martin Podloucký <podlouckymartin@gmail.com>
2022-10-11 23:18:13 +0200

    Přidání veganského označení

--- src/index.jsx
+++ src/index.jsx
@@ -7,42 +7,62 @@ const toppings = [
   {
     name: 'Pepperoni',
     price: 1,
+    vegan: false,
   },
   {
     name: 'Mushroom',
     price: 0.35,
+    vegan: true,
   },
   {
     name: 'Extra cheese',
     price: 0.5,
+    vegan: false,
   },
   {
     name: 'Sausage',
     price: 0.8,
+    vegan: false,
   },
   {
     name: 'Onion',
     price: 0.25,
+    vegan: true,
   },
   {
     name: 'Black olives',
     price: 0.65,
+    vegan: true,
   },
   {
     name: 'Green pepper',
     price: 0.55,
+    vegan: true,
   },
   {
     name: 'Fresh garlic',
     price: 0.1,
+    vegan: true,
   },
   {
     name: 'Tomato',
     price: 0.5,
+    vegan: true,
   },
   {
     name: 'Fresh basil',
     price: 0.15,
+    vegan: true,
+  },
+  {
+    name: 'Pineapple',
+    price: 0.7,
+    vegan: true,
+  },
+  {
+    name: 'Prosciutto',
+    price: 1.2,
+    vegan: false,
   },
 ];
 
