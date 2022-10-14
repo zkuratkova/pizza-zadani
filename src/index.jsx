c3215cea622dff85273e64deae0488262677765f
Martin Podloucký <podlouckymartin@gmail.com>
2022-10-14 09:34:16 +0200

    Přidání property selected

--- src/index.jsx
+++ src/index.jsx
@@ -8,61 +8,73 @@ const toppings = [
     name: 'Pepperoni',
     price: 1,
     vegan: false,
+    selected: false,
   },
   {
     name: 'Mushroom',
     price: 0.35,
     vegan: true,
+    selected: false,
   },
   {
     name: 'Extra cheese',
     price: 0.5,
     vegan: false,
+    selected: false,
   },
   {
     name: 'Sausage',
     price: 0.8,
     vegan: false,
+    selected: false,
   },
   {
     name: 'Onion',
     price: 0.25,
     vegan: true,
+    selected: false,
   },
   {
     name: 'Black olives',
     price: 0.65,
     vegan: true,
+    selected: false,
   },
   {
     name: 'Green pepper',
     price: 0.55,
     vegan: true,
+    selected: false,
   },
   {
     name: 'Fresh garlic',
     price: 0.1,
     vegan: true,
+    selected: false,
   },
   {
     name: 'Tomato',
     price: 0.5,
     vegan: true,
+    selected: false,
   },
   {
     name: 'Fresh basil',
     price: 0.15,
     vegan: true,
+    selected: false,
   },
   {
     name: 'Pineapple',
     price: 0.7,
     vegan: true,
+    selected: false,
   },
   {
     name: 'Prosciutto',
     price: 1.2,
     vegan: false,
+    selected: false,
   },
 ];
 
